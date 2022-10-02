95cd6631eda7ab47e290d6e193e00210961df104
shubham rajput <shubham.sr959@gmail.com>
2022-10-03 03:32:04 +0530

    Updates

---
+++ precache-manifest.d9817990808954b1746ef65b34a06e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebc7ef1e326ef6ba7168",
+    "url": "/my-apps/static/css/main.28acb54e.chunk.css"
+  },
+  {
+    "revision": "ebc7ef1e326ef6ba7168",
+    "url": "/my-apps/static/js/main.ebc7ef1e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-apps/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/my-apps/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "0094c05d4e9e7a94aaa7",
+    "url": "/my-apps/static/js/runtime~main.0094c05d.js"
+  },
+  {
+    "revision": "81856d2593ddf0c1b5eddaa74b737b69",
+    "url": "/my-apps/index.html"
+  }
+];
